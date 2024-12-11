e7b8749c2c270253db2e12ddde8fde887efac47e
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-12-11 09:41:28 +0100

    persistencia con archivos de preferencias clave/valor

--- app/src/main/java/com/cesur/app03/MainActivity.java
+++ app/src/main/java/com/cesur/app03/MainActivity.java
@@ -2,24 +2,50 @@ package com.cesur.app03;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Button botonMusica;
     private Button botonVideo;
     private SharedPreferences misPreferencias;
+    private Switch modoOscuro;
+    private ConstraintLayout main;
+    private TextView titulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        misPreferencias = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = misPreferencias.edit();
+
+
         botonMusica = (Button) findViewById(R.id.botonMusica);
         botonVideo = (Button) findViewById(R.id.botonVideo);
+        modoOscuro = (Switch) findViewById(R.id.modoOscuro);
+        main = (ConstraintLayout) findViewById(R.id.main);
+        titulo = (TextView) findViewById(R.id.titulo);
+
+        if (misPreferencias.getBoolean("modoOscuro", false)) {
+            main.setBackgroundColor(Color.DKGRAY);
+            titulo.setTextColor(Color.WHITE);
+            modoOscuro.setTextColor(Color.WHITE);
+            modoOscuro.setChecked(true);
+        } else {
+            main.setBackgroundColor(Color.WHITE);
+            titulo.setTextColor(Color.BLACK);
+            modoOscuro.setTextColor(Color.BLACK);
+            modoOscuro.setChecked(false);
+        }
 
         botonMusica.setOnClickListener(
                 new View.OnClickListener() {
@@ -40,8 +66,24 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        misPreferencias = getSharedPreferences("prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = misPreferencias.edit();
+        modoOscuro.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (modoOscuro.isChecked()) {
+                            main.setBackgroundColor(Color.DKGRAY);
+                            titulo.setTextColor(Color.WHITE);
+                            modoOscuro.setTextColor(Color.WHITE);
+                        } else {
+                            main.setBackgroundColor(Color.WHITE);
+                            titulo.setTextColor(Color.BLACK);
+                            modoOscuro.setTextColor(Color.BLACK);
+                        }
+                        editor.putBoolean("modoOscuro", modoOscuro.isChecked());
+                        editor.apply();
+                    }
+                }
+        );
 
 
     }
